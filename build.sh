086a4fb2bbd5e228e8d88d39dde735386d4c6d62
Mike Gran <spk121@yahoo.com>
2013-04-09 21:13:08 -0700

    Change build script to use texi2any
    Moving from Texinfo 4.x to 5.x
    
    * build.sh: modified

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-makeinfo --html --output=html g100.texi
-texi2dvi g100.texi
-texi2pdf g100.texi
+
+## Texinfo 4.x
+# makeinfo --html --output=html g100.texi
+# texi2dvi g100.texi
+# texi2pdf g100.texi
+# rm g100.aux g100.cp g100.fn g100.ky g100.log g100.pg g100.toc g100.tp g100.vr
+
+## Texinfo 5.x
+texi2any --html --output=html --css-include=g100.css g100.texi
+texi2any --dvi g100.texi
+texi2any --pdf g100.texi
 rm g100.aux g100.cp g100.fn g100.ky g100.log g100.pg g100.toc g100.tp g100.vr
